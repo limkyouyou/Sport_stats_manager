5864c4c7b4e3ce12a4e66098c60ef571b3e3950c
Kyo young Lim <limkyouyou@gmail.com>
2025-06-08 20:01:47 -0400

    Change name to main.py and create placeholder constructor

---
+++ main.py
@@ -0,0 +1,8 @@
+
+class SportsManagerApp:
+    def __init__(self):
+        # Initialize util class, GUI class, anda data class
+        self.util = None  # Placeholder for utility class
+        self.gui = None  # Placeholder for GUI class
+        self.data = None  # Placeholder for data class
+        
